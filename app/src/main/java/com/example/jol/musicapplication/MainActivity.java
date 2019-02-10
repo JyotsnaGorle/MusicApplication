f5034fd59c8bc9df4b83e08f12e362fec50b7da7
Jyotsna Gorle <jyotsnagorle@gmail.com>
2019-02-10 15:20:50 +0100

    basic app and navigation to search activity

---
+++ app/src/main/java/com/example/jol/musicapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.jol.musicapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.search_option:
+                startSearchActivity();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startSearchActivity() {
+        Intent searchActivityIntent = new Intent(MainActivity.this, SearchActivity.class);
+        MainActivity.this.startActivity(searchActivityIntent);
+    }
+}
