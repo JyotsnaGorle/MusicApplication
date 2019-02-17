79aa33d2176c7133420ba291f31049081f59b67d
Jyotsna Gorle <jyotsnagorle@gmail.com>
2019-02-17 21:12:53 +0100

    handle no music in local

--- app/src/main/java/com/example/jol/musicapplication/fragments/SavedAlbumListFragment.java
+++ app/src/main/java/com/example/jol/musicapplication/fragments/SavedAlbumListFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.jol.musicapplication.Models.SavedAlbum;
 import com.example.jol.musicapplication.R;
@@ -21,13 +22,14 @@ public class SavedAlbumListFragment extends Fragment {
 
     private RecyclerView savedAlbums;
     private SQLiteDatabaseHandler db;
+    private TextView emptyList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_saved_album,
                 container, false);
-
+        emptyList = view.findViewById(R.id.empty_list);
         savedAlbums = view.findViewById(R.id.saved_albums);
         savedAlbums.setHasFixedSize(true);
         db = SQLiteDatabaseHandler.getInstance(this.getContext());
@@ -38,8 +40,15 @@ public class SavedAlbumListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ArrayList<SavedAlbum> allMyMusic = db.allMyMusic();
-        if (allMyMusic.size() > 0 )
+        if (allMyMusic.size() > 0 ) {
             populateList(allMyMusic);
+            emptyList.setVisibility(View.GONE);
+        }
+        else {
+            emptyList.setText("No Locally saved Albums");
+            emptyList.setVisibility(View.VISIBLE);
+        }
+
     }
 
     private void populateList(ArrayList<SavedAlbum> savedSavedAlbums) {
